8fc88cad0ee78c2644cc7149aa9e7eafe7c64497
matobat <matobat@gmail.com>
2006-05-05 02:16:24 +0000

    S2ComponentMapの対応
    git-svn-id: https://www.seasar.org/svn/sandbox/s2xwork/trunk@22 f96b1df8-7065-4131-8f9c-705339fe6063

--- s2-xwork-example-webwork/src/main/java/org/seasar/xwork/example/webwork/AddAction.java
+++ s2-xwork-example-webwork/src/main/java/org/seasar/xwork/example/webwork/AddAction.java
@@ -5,34 +5,22 @@ import com.opensymphony.xwork.ActionSupport;
 public class AddAction extends ActionSupport {
 	private AddService addService;
 
-	private int arg1;
-
-	private int arg2;
-
-	private int result;
+	private AddDto addDto;
 
 	public AddAction(AddService addService) {
 		this.addService = addService;
 	}
 
-	public void setArg1(int arg1) {
-		this.arg1 = arg1;
-	}
-
-	public void setArg2(int arg2) {
-		this.arg2 = arg2;
-	}
-
-	public int getResult() {
-		return result;
-	}
-
 	public String execute() throws Exception {
-		result = addService.add(arg1, arg2);
+		addDto.setResult(addService.add(addDto.getArg1(), addDto.getArg2()));
 		return SUCCESS;
 	}
 
 	public void setAddService(AddService addService) {
 		this.addService = addService;
+	}
+
+	public void setAddDto(AddDto addDto) {
+		this.addDto = addDto;
 	}
 }
