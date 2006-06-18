feb1699626913f3842ec944b99915077151699ed
matobat <matobat@gmail.com>
2006-06-18 09:53:17 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2xwork/trunk@53 f96b1df8-7065-4131-8f9c-705339fe6063

--- s2-xwork-example-webwork/src/main/java/org/seasar/xwork/example/webwork/AddAction.java
+++ s2-xwork-example-webwork/src/main/java/org/seasar/xwork/example/webwork/AddAction.java
@@ -1,23 +1,30 @@
 package org.seasar.xwork.example.webwork;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.seasar.xwork.annotation.Param;
 import org.seasar.xwork.annotation.Result;
 import org.seasar.xwork.annotation.XWorkAction;
 
 import com.opensymphony.xwork.ActionSupport;
 
-@XWorkAction(name = "test", interceptorRef = "", result = @Result(type = "velocity", param = @Param(name = "location", value = "add_success.vm")))
+@XWorkAction(name = "add", result = @Result(type = "velocity", param = @Param(name = "location", value = "add_success.vm")))
 public class AddAction extends ActionSupport {
 	private AddService addService;
 
 	private AddDto addDto;
+	
+	private Log log = LogFactory.getLog(AddAction.class);
 
 	public AddAction(AddService addService) {
 		this.addService = addService;
 	}
 
 	public String execute() throws Exception {
+		log.debug("arg1:"+addDto.getArg1());
+		log.debug("arg2:"+addDto.getArg2());
 		addDto.setResult(addService.add(addDto.getArg1(), addDto.getArg2()));
+		log.debug("result:"+addDto.getResult());
 		return SUCCESS;
 	}
 
